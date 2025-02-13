b25913cd2e1c30caee4d5dd61cc4a5c1d5652ff0
Andrey Synyuk <andrey.sinyuk@triogeeks.com>
2025-02-13 12:04:33 +0200

    add to basket option change products count

--- src/Components/Basket/Basket.jsx
+++ src/Components/Basket/Basket.jsx
@@ -1,6 +1,6 @@
 import './Basket.scss';
 import { useSelector, useDispatch } from 'react-redux';
-import {addToCard, removeAllFromCards, removeToCard} from '../../store/reducers/basketReducer';
+import {removeAllFromCards, removeToCard, incrementProducts, decrementProduct } from '../../store/reducers/basketReducer';
 import { useMyContext } from "../../App";
 import { useState, useEffect } from "react";
 import { ref, get } from "firebase/database";
@@ -18,6 +18,7 @@ function Basket() {
         get(dbRef).then((snapshot) => {
             if (snapshot.exists()) {
                 const allProducts = snapshot.val();
+                console.log(productsInBasket);
                 const selectedProducts = productsInBasket.map((item) => ({
                     ...allProducts[item.id],
                     count: item.count,
@@ -41,7 +42,6 @@ function Basket() {
     const deleteItemHandler = (e) => {
         const productId = e.target.closest(".product-basket").getAttribute("data-id");
         const productCounter = e.target.closest(".product-basket").querySelector(".counter").innerText;
-
         //console.log(productCounter);
         setCounter(prev => Number(prev) - productCounter);
         dispatch(removeToCard(productId));
@@ -52,24 +52,43 @@ function Basket() {
         setCounter(0);
     }
 
+    const decrementHendler = (e) => {
+        const productId = e.target.closest(".product-basket").getAttribute("data-id");
+        console.log(productId);
+        dispatch(decrementProduct(productId));
+        setCounter(prev => Number(prev) - 1 );
+    }
+
+    const incrementHendler = (e) => {
+        const productId = e.target.closest(".product-basket").getAttribute("data-id");
+        dispatch(incrementProducts(productId));
+        setCounter(prev => Number(prev) + 1 );
+    }
+
     return (
         <section className="container">
             {products.length > 0 ? (
                 products.map((product) => (
                     <div className="product-basket" data-id={`${product.id}`} key={`${product.id}`}>
-                        <div className="counter">{product.count}</div>
                         <div className="product-basket-image">
                             <img src={`${process.env.PUBLIC_URL}/${product.pic}`} alt=""/>
                         </div>
                         <div className="product-basket-body">
                             <div className="product-basket-company">{product.company}</div>
                             <div className="product-basket-name">{product.name}</div>
-                            <div className="product-basket-price">{product.price}</div>
                             <div className="product-basket-delete" onClick={deleteItemHandler}>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24px" height="24px">
                                     <path d="M 4.7070312 3.2929688 L 3.2929688 4.7070312 L 10.585938 12 L 3.2929688 19.292969 L 4.7070312 20.707031 L 12 13.414062 L 19.292969 20.707031 L 20.707031 19.292969 L 13.414062 12 L 20.707031 4.7070312 L 19.292969 3.2929688 L 12 10.585938 L 4.7070312 3.2929688 z"/>
                                 </svg>
                             </div>
+                        </div>
+                        <div className="product-basket-footer">
+                            <div className="counter-wrapper">
+                                <button onClick={decrementHendler} className="count-decrement">-</button>
+                                <div className="counter">{product.count}</div>
+                                <button onClick={incrementHendler} className="count-increment">+</button>
+                            </div>
+                            <div className="product-basket-price">{product.price}</div>
                         </div>
                     </div>
                 ))
--- src/store/reducers/basketReducer.js
+++ src/store/reducers/basketReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import products from "../../Components/Products/Products";
 
 export const basketReducer = createSlice({
     name: 'basket',
@@ -23,11 +24,29 @@ export const basketReducer = createSlice({
         removeAllFromCards: (state, action) => {
             console.log('removeAllFromCards');
             state.products = [];
+        },
+        decrementProduct: (state, action) => {
+            const counter = state.products.find(product => product.id === action.payload);
+            const index = state.products.findIndex(product => product.id === action.payload);
+            //console.log(counter.count);
+            if (counter) {
+                counter.count < 2 ? (counter.count = 0,  state.products.splice(index, 1)) : counter.count -= 1;
+            } else {
+                state.products.push({ id: action.payload, count: 1 });
+            }
+        },
+        incrementProducts: (state, action) => {
+            const counter = state.products.find(product => product.id === action.payload);
+            if (counter) {
+                counter.count += 1;
+            } else {
+                state.products.push({ id: action.payload, count: 1 });
+            }
         }
 
     }
 })
 
-export const { addToCard, removeToCard, removeAllFromCards } = basketReducer.actions
+export const { addToCard, removeToCard, removeAllFromCards, decrementProduct, incrementProducts } = basketReducer.actions
 
 export default basketReducer.reducer
